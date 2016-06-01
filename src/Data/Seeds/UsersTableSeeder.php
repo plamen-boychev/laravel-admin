772955338f9fb0066dd93ad0469e17d77bd510fb
Vagrant Default User <vagrant@debian-jessie.vagrantup.com>
2016-06-01 08:49:08 +0000

    Resolving namespace alias issues

---
+++ src/Data/Seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace LAdmin\Data\Seeds;
+
+use DB;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'first_name' => 'Admin',
+            'last_name' => 'Admin',
+            'email' => 'admin@ladmin.dev',
+            'password' => bcrypt('adminpass'),
+        ]);
+    }
+
+}
