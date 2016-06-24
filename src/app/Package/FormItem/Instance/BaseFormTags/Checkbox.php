15c52bbd9919c0b2f037204304ec4c2cd3f7f96e
Vagrant Default User <vagrant@debian-jessie.vagrantup.com>
2016-06-24 14:41:39 +0000

    Checkbox - setting checked flag value

--- src/app/Package/FormItem/Instance/BaseFormTags/Checkbox.php
+++ src/app/Package/FormItem/Instance/BaseFormTags/Checkbox.php
@@ -3,7 +3,11 @@
 namespace LAdmin\Package\FormItem\Instance\BaseFormTags;
 
 use LAdmin\Package\PrintableInterface;
+use LAdmin\Package\FormItem\FormItemInterface;
 
+/**
+ * @todo Checked attribute if is checked
+ */
 class Checkbox extends Text
 {
 
@@ -21,5 +25,35 @@ class Checkbox extends Text
      * {@inheritdoc}
      */
     protected $templateFileName = 'checkbox';
+
+    /**
+     * Checked attribute setter
+     *
+     * @param  bool $checked
+     *
+     * @return FormItemInterface
+     */
+    public function setChecked(bool $checked) : FormItemInterface
+    {
+        if ($checked === true) {
+            $this->addAttribute('checked', 'checked');
+        } else {
+            $this->removeAttribute('checked');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Checked attribute setter
+     *
+     * @param  null
+     *
+     * @return bool
+     */
+    public function getChecked() : bool
+    {
+        return (bool) $this->getAttribute('checked', false);
+    }
 
 }
